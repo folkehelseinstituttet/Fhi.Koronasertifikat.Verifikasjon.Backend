a3ef330bc2388133359de01175ce4441d5f9e7d7
nc-005 <nc-005@netcompany.com>
2021-06-03 10:59:07 +0200

    Added possibility for using KayVault for National Backend TLS client certificate for mutual TLS authentification to EU DGCG

--- FHICORC.Backend/FHICORC.Integrations.DGCGateway/Services/DGCGClient.cs
+++ FHICORC.Backend/FHICORC.Integrations.DGCGateway/Services/DGCGClient.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using System.Threading.Tasks;
 using FHICORC.Application.Models;
 using Newtonsoft.Json;
@@ -6,6 +7,9 @@ using FHICORC.Application.Models.Options;
 using System.Security.Cryptography.X509Certificates;
 using RestSharp;
 using System.Net;
+using Azure.Identity;
+using Azure.Security.KeyVault.Certificates;
+using Azure.Security.KeyVault.Secrets;
 using FHICORC.Integrations.DGCGateway.Services.Interfaces;
 using Microsoft.Extensions.Logging;
 
@@ -32,7 +36,7 @@ namespace FHICORC.Integrations.DGCGateway.Services
             ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls12;
 
             //Fetch TLS certificate 
-            X509Certificate2 certificate = new X509Certificate2(_certificateOptions.NBTlsCertificatePath);
+            var certificate = await FetchTlsCertificate();
 
             client.ClientCertificates = new X509CertificateCollection() { certificate };
             client.Proxy = new WebProxy();
@@ -59,6 +63,31 @@ namespace FHICORC.Integrations.DGCGateway.Services
                 resultDto.TrustListItems = parsedResponse.ToList();
             }
             return resultDto;
+        }
+
+        private async Task<X509Certificate2> FetchTlsCertificate()
+        {
+            if (!string.IsNullOrEmpty(_certificateOptions.NBTlsKeyVaultUrl) &&
+                !string.IsNullOrEmpty(_certificateOptions.NBTlsKeyVaultCertificateName))
+            {
+                var keyVaultUri = new Uri(_certificateOptions.NBTlsKeyVaultUrl);
+                var certificateName = _certificateOptions.NBTlsKeyVaultCertificateName;
+                var credential = new DefaultAzureCredential();
+
+                var certClient = new CertificateClient(keyVaultUri, credential);
+                var secretClient = new SecretClient(keyVaultUri, credential);
+                var certificate = (await certClient.GetCertificateAsync(certificateName)).Value;
+
+                Uri secretId = certificate.SecretId;
+                var segments = secretId.Segments;
+                string secretName = segments[2].Trim('/');
+                string version = segments[3].TrimEnd('/');
+
+                var secret = (await secretClient.GetSecretAsync(secretName, version)).Value;
+                return new X509Certificate2(Convert.FromBase64String(secret.Value));
+            }
+
+            return new X509Certificate2(_certificateOptions.NBTlsCertificatePath, _certificateOptions.NBTlsCertificatePassword);
         }
     }
 }
