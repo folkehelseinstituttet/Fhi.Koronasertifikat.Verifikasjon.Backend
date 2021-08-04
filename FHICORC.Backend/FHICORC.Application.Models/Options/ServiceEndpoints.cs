5a48446be579febb3488d8ea9ce0e1764b20b373
NC-005 <netcompany005@gmail.com>
2021-08-04 09:59:34 +0200

    UK public key integration

--- FHICORC.Backend/FHICORC.Application.Models/Options/ServiceEndpoints.cs
+++ FHICORC.Backend/FHICORC.Application.Models/Options/ServiceEndpoints.cs
@@ -4,5 +4,6 @@ namespace FHICORC.Application.Models.Options
     public class ServiceEndpoints
     {
         public string DGCGTrustListEndpoint { get; set; }
+        public string UKTrustListEndpoint { get; set; }
     }
 }
