f358d806b5f4d4b95e76b44732b4f2f68c8941c4
Tomasz Wandel <tomw@netcompany.com>
2021-10-25 11:27:28 +0200

    CountriesReport feature

---
+++ FHICORC.Backend/FHICORC.Application.Repositories/Interfaces/ICoutriesReportRepository.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace FHICORC.Application.Repositories.Interfaces
+{
+    public interface ICountriesReportRepository
+    {
+        Task<IList<string>> GetAllCountries();
+    }
+}
