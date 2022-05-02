1a4109f944be24d5ff671106b77f26d005be1d1a
gitGut01 <silaseei@hotmail.com>
2022-05-02 08:46:17 +0200

    Solved Merge with dev

--- FHICORC.Backend/FHICORC.Integrations.DGCGateway/Services/DGCGRevocationService.cs
+++ FHICORC.Backend/FHICORC.Integrations.DGCGateway/Services/DGCGRevocationService.cs
@@ -35,7 +35,6 @@ namespace FHICORC.Integrations.DGCGateway.Services
                     AddToDatabase(rb, revocationHashList);
                 }
                 catch (Exception e) { }
-             
             }
         }
 
