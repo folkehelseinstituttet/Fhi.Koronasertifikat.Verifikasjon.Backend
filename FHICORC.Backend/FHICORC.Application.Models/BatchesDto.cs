7780d44067317657f1ac9418e0d74deac309f704
Thed Almgren <thedalmgren@hotmail.com>
2022-05-04 13:56:26 +0200

    add BatchesDto and HashesDto

---
+++ FHICORC.Backend/FHICORC.Application.Models/BatchesDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FHICORC.Application.Models
+{
+    public class BatchesDto
+    {
+        public List<Batch> Batches { get; set; }
+    }
+
+    public class Batch
+    {
+        public string BatchId { get; set; }
+        public DateTime Expires { get; set; }
+        public int SuperId { get; set; }
+        public int Count { get; set; }
+    }
+}
---
+++ FHICORC.Backend/FHICORC.Application.Models/HashesDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FHICORC.Application.Models
+{
+    public class HashesDto
+    {
+        public List<Hash> Hashes { get; set; }
+    }
+
+    public class Hash
+    {
+        public int Id { get; set; }
+        public string BatchId { get; set; }
+        public string HashInfo { get; set; }
+    }
+}
