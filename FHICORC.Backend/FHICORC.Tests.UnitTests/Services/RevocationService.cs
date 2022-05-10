0b6d359c29ae15c28479776aa7d0e5c6a9eeacaa
gitGut01 <silaseei@hotmail.com>
2022-05-10 13:41:43 +0200

    Added component test to unitTest project so it runs in the pipeline

---
+++ FHICORC.Backend/FHICORC.Tests.UnitTests/Services/RevocationService.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FHICORC.Tests.UnitTests.Services
+{
+    [Category("Unit")]
+    public class RevocationService : SeedDb
+    {
+
+        [Test]
+        public void IsSingleHashInSuperFilter()
+        {
+            var result = _revocationService.ContainsCertificate("p8dtvN0/9YKtwgGAxvsvBg==", "RO");
+            Assert.True(result);
+        }
+
+        [Test]
+        public void AreAllHashesInSuperFilter()
+        {
+            foreach (var hash in _coronapassContext.RevocationHash)
+            {
+                var country = _coronapassContext.RevocationBatch.Find(hash.BatchId).Country;
+                var result = _revocationService.ContainsCertificate(hash.Hash, country);
+             
+                Assert.True(result);
+            }
+        }
+
+
+
+    }
+}
