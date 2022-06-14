ef06473be8322533845f83f4dfca0f6fe070d01f
gitGut01 <silaseei@hotmail.com>
2022-06-14 13:47:38 +0200

    Updatet mobileUtil to match the app, for testing purposes

--- FHICORC.Backend/FHICORC.Tests.ComponentTests/MobileDemo/MobileDemoTests.cs
+++ FHICORC.Backend/FHICORC.Tests.ComponentTests/MobileDemo/MobileDemoTests.cs
@@ -11,11 +11,14 @@ namespace FHICORC.Tests.ComponentTests.DGCGComponentTests
     {
       
 
-        [TestCase("J4PCK4sFs63kH/EeP7+C3A==")]
+        //[TestCase("J4PCK4sFs63kH/EeP7+C3A==")]
+
         //[TestCase("YWFhYWFhYWFhYWFhYWFhYQ==")]
         //[TestCase("p8dtvN0/9YKtwgGAxvsvBg==")]
+        [TestCase("+hkApE6qvfhfb95y/Jjx/w==")]
         public void IsSingleHashInSuperFilter(string str) {
-            var result = MobileUtil.ContainsCertificateFilterMobile("RO", str, _coronapassContext, bloomBucketService.GetBloomFilterBucket());
+            var superFilter = _coronapassContext.RevocationSuperFilter.Where(x => x.SuperCountry=="CZ").ToList();
+            var result = MobileUtil.ContainsCertificateFilterMobile(str, str, superFilter, bloomBucketService.GetBloomFilterBucket().ToList());
 
             Assert.True(result);
         }
@@ -24,10 +27,12 @@ namespace FHICORC.Tests.ComponentTests.DGCGComponentTests
         public void AreAllHashesInSuperFilter()
         {
             var cnt = 0;
+            var superFilter = _coronapassContext.RevocationSuperFilter.ToList();
             foreach (var hash in _coronapassContext.RevocationHash) {
                 var country = _coronapassContext.RevocationBatch.Find(hash.BatchId).Country;
                 cnt += 1;
-                var result = MobileUtil.ContainsCertificateFilterMobile(country, hash.Hash, _coronapassContext, bloomBucketService.GetBloomFilterBucket());
+                
+                var result = MobileUtil.ContainsCertificateFilterMobile(hash.Hash, hash.Hash, superFilter, bloomBucketService.GetBloomFilterBucket().ToList());
                 Assert.True(result);
             }
         }
